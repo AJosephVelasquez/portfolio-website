85e8d9bfc7f679bc107a6f04cdf74690db1e585e
Alain Velasquez <velasquezalain19@gmail.com>
2023-06-22 08:54:07 +0800

    First Commit

--- src/data.jsx
+++ src/data.jsx
@@ -33,7 +33,7 @@ export const links = [
         id: 1,
         name: 'Home',
         icon: <FaHome className='nav-icon' />,
-        path: '/'
+        path: '/portfolio-website'
     },
 
     {
